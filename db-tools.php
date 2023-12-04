5f6488fa567c525103298cbdc922377dfa8edb8f
3041TAN-05 <3041TAN-05@boronkay.local>
2023-12-04 09:21:59 +0100

    update_get_delete_makers

--- db-tools.php
+++ db-tools.php
@@ -13,4 +13,42 @@ function insertMakers($mysqli, $makers, $truncate = false)
     }
     return $result;
 }
+
+function updateMaker($mysqli, $data) {
+    $result = $mysqli->query("UPDATE makers SET maker name = {$data['name']}");
+    if (!$result) {
+        echo "hiba történt a {$data['name']} beszúrása közben";
+        return $result;    
+    }
+
+    $maker = getMakerByName($mysqli, $data['name']);
+    return $maker;
+}
+
+function getMaker($mysqli, $id){
+    $result = $mysqli->query("SELECT  * FROM makers WHERE id=$id");
+    $maker = $result->fetch_assoc();
+
+    return $maker;
+}
+
+function getMakerByName($mysqli, $name){
+    $result = $mysqli->query("SELECT * FROM makers WHERE name=$name");
+    $maker = $result->fetch_assoc();
+
+    return $maker;
+}
+
+function deleteMaker($mysqli, $id){
+    $result = $mysqli->query("DELETE makers WHERE id=$id");
+    
+    return $result;
+}
+
+function getAllMakers($mysqli){
+    $result = $mysqli->query("SELECT * FROM makers");
+    $maker = $result->fetch_assoc();
+
+    return $maker;
+}
 ?>
